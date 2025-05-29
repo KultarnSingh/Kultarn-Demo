7d9ce97238950b73db372a495e90f19512d5821f
Kultarn <sidhark@sheridancollege.ca>
2025-05-29 09:20:50 -0400

    Create Kultarn.java

---
+++ Kultarn.java
@@ -0,0 +1,23 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Week_4 {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        FileWriter fw = new FileWriter("Kultarn.txt", true);
+        PrintWriter writer = new PrintWriter(fw);
+
+        System.out.println("Enter text to write to the file (type 'exit' to stop):");
+        String input;
+        while (!(input = scanner.nextLine()).equalsIgnoreCase("exit")) {
+            writer.println(input);
+        }
+
+        writer.close();
+        scanner.close();
+        System.out.println("Done writing to the file!");
+    }
+}
